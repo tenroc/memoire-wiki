23e53fe2a571e6ccd6ecdc12942354e1a642604e
Maxime Cornet <maxime.765@gmail.com>
2017-08-01 23:44:45 +0200

    modification du sample
    modification du sample pour ne garder que 100 reseaux

--- wiki_reseaux/ensemble_reseaux/composition_base.R
+++ wiki_reseaux/ensemble_reseaux/composition_base.R
@@ -5,7 +5,11 @@ controversy_data <- as.character(controversy[[1]])
 
 # sample de 200: 
 
-controversy_sample <- sample(controversy_data, 200)
+controversy_sample <- sample(controversy_data, 100)
+
+# export
+
+write(controversy_sample, file="controversy_sample.txt", sep="/n")
 
 #### Recupperer la liste des articles mis en avant #####
 
@@ -14,8 +18,21 @@ featured_data <- as.character(featured[[1]])
 
 # sample de 200:
 
-featured_sample <- sample(featured_data, 200)
+featured_sample <- sample(featured_data, 100)
+
+# export
+
+write(featured_sample, file="featured_sample.txt", sep="/n")
 
 #### Et un sample de 200 pages au hasard ####
 
-page_sample_data <- random_page("en", project="wikipedia", namespaces = 0, limit=200)
+page_sample_data <- random_page("en", project="wikipedia", namespaces = 0, limit=100)
+
+for (i in 1:100){
+  page_sample[i] <- paste(page_sample_data[[i]][1])
+}
+
+# export
+
+write(page_sample, file="page_sample.txt", sep="/n")
+
